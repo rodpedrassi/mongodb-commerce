d92fd7f7785209054ca2776ecc55ef6538ea8e0c
rodpedrassi <rodpedrassi@gmail.com>
2023-02-07 16:50:04 -0300

    15 - Adicione o campo avaliacao em todos os documentos da coleção e efetue alterações nesse campo

---
+++ challenges/desafio15.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany({},{$set: {avaliacao: NumberInt(0)}});
+
+db.produtos.updateMany(
+    {tags: {$eq:'bovino'}},
+    {$inc: {avaliacao: 5}}
+);
+
+db.produtos.updateMany(
+    {tags: {$eq:'ave'}},
+    {$inc: {avaliacao: 3}}
+);
+
+db.produtos.find({}, {nome: 1, _id: 0, avaliacao: 1});
